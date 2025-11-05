aa30fc59cb812bd29c1665f4c509f86aba7eb990
Simon Chatterjee <code@chatts.net>
2025-11-05 18:45:01 +0000

    Add Measure.voltas to plugin API

--- src/engraving/api/v1/elements.cpp
+++ src/engraving/api/v1/elements.cpp
@@ -22,6 +22,8 @@
 
 #include "elements.h"
 
+#include <algorithm>
+
 #include "engraving/dom/chord.h"
 #include "engraving/dom/guitarbend.h"
 #include "engraving/dom/measure.h"
@@ -390,6 +392,79 @@ void Chord::remove(apiv1::EngravingItem* wrapped)
     } else {
         chord()->score()->deleteItem(s);     // Create undo op and remove the element.
     }
+}
+
+QQmlListProperty<Spanner> Measure::voltas()
+{
+    updateVoltasCache();
+    return QQmlListProperty<Spanner>(this, this, &Measure::voltaCount, &Measure::voltaAt);
+}
+
+qsizetype Measure::voltaCount(QQmlListProperty<Spanner>* list)
+{
+    auto* measureWrapper = static_cast<Measure*>(list->data);
+    return measureWrapper ? static_cast<qsizetype>(measureWrapper->m_cachedVoltas.size()) : 0;
+}
+
+Spanner* Measure::voltaAt(QQmlListProperty<Spanner>* list, qsizetype index)
+{
+    auto* measureWrapper = static_cast<Measure*>(list->data);
+    if (!measureWrapper) {
+        return nullptr;
+    }
+
+    if (index < 0 || static_cast<size_t>(index) >= measureWrapper->m_cachedVoltas.size()) {
+        return nullptr;
+    }
+
+    return wrap<Spanner>(measureWrapper->m_cachedVoltas[static_cast<size_t>(index)], Ownership::SCORE);
+}
+
+void Measure::updateVoltasCache() const
+{
+    m_cachedVoltas.clear();
+
+    const mu::engraving::Measure* muMeasure = measure();
+    if (!muMeasure) {
+        return;
+    }
+
+    const mu::engraving::Score* muScore = muMeasure->score();
+    if (!muScore) {
+        return;
+    }
+
+    const int startTick = muMeasure->tick().ticks();
+    const int endTick = muMeasure->endTick().ticks() - 1;
+
+    if (endTick < startTick) {
+        return;
+    }
+
+    const auto& intervals = muScore->spannerMap().findOverlapping(startTick, endTick);
+    for (const auto& interval : intervals) {
+        mu::engraving::Spanner* spanner = interval.value;
+        if (!spanner) {
+            continue;
+        }
+
+        if (spanner->type() != mu::engraving::ElementType::VOLTA) {
+            continue;
+        }
+
+        if (spanner->startMeasure() != muMeasure) {
+            continue;
+        }
+
+        m_cachedVoltas.push_back(spanner);
+    }
+
+    std::sort(m_cachedVoltas.begin(), m_cachedVoltas.end(), [](const mu::engraving::Spanner* lhs, const mu::engraving::Spanner* rhs) {
+        if (lhs->tick() == rhs->tick()) {
+            return lhs < rhs;
+        }
+        return lhs->tick() < rhs->tick();
+    });
 }
 
 EngravingItem* Measure::vspacerUp(int staffIdx)
--- src/engraving/api/v1/elements.h
+++ src/engraving/api/v1/elements.h
@@ -24,6 +24,7 @@
 #include "scoreelement.h"
 
 #include <QQmlListProperty>
+#include <vector>
 
 #include "engraving/dom/engravingitem.h"
 #include "engraving/dom/arpeggio.h"
@@ -1739,6 +1740,9 @@ class Measure : public MeasureBase
     Q_PROPERTY(apiv1::Segment * firstSegment READ firstSegment)
     /// The last segment of this measure
     Q_PROPERTY(apiv1::Segment * lastSegment READ lastSegment)
+    /// Volta spanners starting on this measure.
+    /// \since MuseScore 4.7
+    Q_PROPERTY(QQmlListProperty<apiv1::Spanner> voltas READ voltas)
 
 //       Q_PROPERTY(bool         lineBreak         READ lineBreak   WRITE undoSetLineBreak)
 //       Q_PROPERTY(bool         pageBreak         READ pageBreak   WRITE undoSetPageBreak)
@@ -1795,6 +1799,7 @@ public:
     Measure* mmRest() const { return wrap<Measure>(measure()->mmRest(), Ownership::SCORE); }
 
     QQmlListProperty<Segment> segments() { return wrapContainerProperty<Segment>(this, measure()->segments()); }
+    QQmlListProperty<Spanner> voltas();
     /// \endcond
 
     /// Up spacer for a given staff.
@@ -1829,6 +1834,13 @@ public:
     /// \param staffIdx staff to check if stemless
     /// \since MuseScore 4.6
     Q_INVOKABLE bool stemless(int staffIdx);
+
+private:
+    static qsizetype voltaCount(QQmlListProperty<Spanner>* list);
+    static Spanner* voltaAt(QQmlListProperty<Spanner>* list, qsizetype index);
+    void updateVoltasCache() const;
+
+    mutable std::vector<mu::engraving::Spanner*> m_cachedVoltas;
 };
 
 //---------------------------------------------------------
